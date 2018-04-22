0de9afb93d02667c6210e3718aeb2773a9a535b4
walterlv <xblero@gmail.com>
2018-04-22 17:38:51 +0800

    Add project files.

---
+++ EmitDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
