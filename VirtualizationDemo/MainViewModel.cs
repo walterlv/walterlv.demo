413a19d977a26f16559e6014289d194c4cca7203
walterlv <xblero@gmail.com>
2018-04-25 19:56:59 +0800

    x

---
+++ VirtualizationDemo/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Walterlv.Demo.Annotations;
+
+namespace Walterlv.Demo
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollection<ItemViewModel> Items { get; } = new ObservableCollection<ItemViewModel>();
+
+        public MainViewModel()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Items.Add(new ItemViewModel {Value = $"DegofaNira = {i}"});
+            }
+        }
+    }
+
+    public class ItemViewModel : ViewModelBase
+    {
+        public string Value
+        {
+            get => _value;
+            set => SetValue(ref _value, value);
+        }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => SetValue(ref _isSelected, value);
+        }
+
+        private string _value;
+        private bool _isSelected;
+    }
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!Equals(field, value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- VirtualizationDemo/MainWindow.xaml.cs
+++ VirtualizationDemo/MainWindow.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace VirtualizationDemo
+namespace Walterlv.Demo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dataContext = (MainViewModel) DataContext;
+            foreach (var item in dataContext.Items)
+            {
+                item.IsSelected = true;
+            }
         }
     }
 }
