49d8e6360ca77038450ef87f84aab64d8625e260
walterlv <xblero@gmail.com>
2018-09-01 17:18:05 +0800

    Add first fiels.

---
+++ LacihaFabeBemrur/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Walterlv.Demo.RemoteObjects;
+
+namespace LacihaFabeBemrur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private Lindexi _remoteLindexi;
+
+        private Lindexi RemoteLindexi
+        {
+            get
+            {
+                if (_remoteLindexi == null)
+                {
+                    _remoteLindexi = (Lindexi) Activator.GetObject(
+                        typeof(Lindexi),
+                        "tcp://127.0.0.1:50632/order");
+                }
+
+                return _remoteLindexi;
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var info = new ProcessStartInfo("Walterlv.Demo.CrossProcess.exe")
+            {
+            };
+            Process.Start(info);
+
+            var tcpClientChannel = new TcpClientChannel();
+            ChannelServices.RegisterChannel(tcpClientChannel, false);
+        }
+
+        private void Order_Click(object sender, RoutedEventArgs e)
+        {
+            var result = RemoteLindexi.OrderDinner("腐竹排骨");
+            LoggerTextBlock.Text += $"{result}{Environment.NewLine}";
+        }
+    }
+}
--- Walterlv.Demo.CrossProcess/Program.cs
+++ Walterlv.Demo.CrossProcess/Program.cs
@@ -1,4 +1,12 @@
-﻿using CommandLine;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Threading;
+using CommandLine;
+using Walterlv.Demo.RemoteObjects;
 
 namespace Walterlv.Demo.CrossProcess
 {
@@ -6,13 +14,49 @@ namespace Walterlv.Demo.CrossProcess
     {
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.FirstChanceException += OnFirstChanceException;
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed<Options>(Run);
         }
 
+        private static void OnFirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+        {
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+        }
+
         private static void Run(Options option)
         {
+            Console.WriteLine("林德熙点餐系统正在初始化……");
 
+            var thread = new Thread(() =>
+            {
+                var lindexi = new Lindexi();
+                Console.WriteLine("林德熙创建完成");
+                RemotingServices.Marshal(lindexi, "order");
+                Console.WriteLine("林德熙已远端化");
+
+                _channel = CreatChannel();
+                Console.WriteLine("林德熙已进入通道");
+                ChannelServices.RegisterChannel(_channel, false);
+                Console.WriteLine("林德熙所在通道已注册完成");
+            })
+            {
+                IsBackground = true,
+            };
+            thread.Start();
+
+            Console.Read();
+        }
+
+        private static IChannel _channel;
+
+        private static IChannel CreatChannel()
+        {
+            var tcpServerChannel = new TcpServerChannel(50632);
+            return tcpServerChannel;
         }
     }
 }
