e4aeb15f9f9cd65065ad10d46137072a1c622229
walterlv <xblero@gmail.com>
2018-09-07 12:33:28 +0800

    Add project files.

---
+++ Walterlv.Windows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
