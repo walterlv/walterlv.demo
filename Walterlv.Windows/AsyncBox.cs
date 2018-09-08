8278f054b73874a7fc1a3a7e1da9b47851bbb020
walterlv <xblero@gmail.com>
2018-09-08 17:34:53 +0800

    Add child.

--- Walterlv.Windows/AsyncBox.cs
+++ Walterlv.Windows/AsyncBox.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Markup;
 using System.Windows.Threading;
+using Walterlv.Annotations;
 using Walterlv.Demo;
 using Walterlv.Demo.Utils.Threading;
 using DispatcherDictionary = System.Collections.Concurrent.ConcurrentDictionary<System.Windows.Threading.Dispatcher, Walterlv.Demo.Utils.Threading.DispatcherAsyncOperation<System.Windows.Threading.Dispatcher>>;
 
 namespace Walterlv.Windows
 {
+    [ContentProperty(nameof(Child))]
     public class AsyncBox : FrameworkElement
     {
         /// <summary>
@@ -17,53 +19,83 @@ namespace Walterlv.Windows
         /// </summary>
         private static readonly DispatcherDictionary RelatedAsyncDispatchers = new DispatcherDictionary();
 
+        [CanBeNull]
+        private UIElement _child;
+
+        [CanBeNull]
+        private Type _loadingViewType;
+
         public AsyncBox()
         {
             Loaded += OnLoaded;
         }
 
+        [CanBeNull]
+        public UIElement Child
+        {
+            get => _child;
+            set
+            {
+                if (Equals(_child, value)) return;
+
+                _child = value;
+            }
+        }
+
+        [NotNull]
+        public Type LoadingViewType
+        {
+            get
+            {
+                if (_loadingViewType == null)
+                {
+                    throw new InvalidOperationException(
+                        $"在 {nameof(AsyncBox)} 显示之前，必须先为 {nameof(LoadingViewType)} 设置一个 {nameof(UIElement)} 作为 Loading 视图。");
+                }
+
+                return _loadingViewType;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(LoadingViewType));
+                }
+
+                if (_loadingViewType != null)
+                {
+                    throw new ArgumentException($"{nameof(LoadingViewType)} 只允许被设置一次。", nameof(value));
+                }
+
+                _loadingViewType = value;
+            }
+        }
+
         /// <summary>
         /// 返回一个可等待的用于显示异步 UI 的后台 UI 线程调度器。
         /// </summary>
+        [NotNull]
         private DispatcherAsyncOperation<Dispatcher> GetAsyncDispatcherAsync() => RelatedAsyncDispatchers.GetOrAdd(
             Dispatcher, dispatcher => UIDispatcher.RunNewAsync("AsyncBox"));
 
+        [NotNull]
+        private UIElement CreateLoadingView()
+        {
+            var instance = Activator.CreateInstance(LoadingViewType);
+            if (instance is UIElement element)
+            {
+                return element;
+            }
+
+            throw new InvalidOperationException($"{LoadingViewType} 必须是 {nameof(UIElement)} 类型");
+        }
+
         private async void OnLoaded(object sender, RoutedEventArgs e)
         {
-            var watch = new Stopwatch();
+            if (DesignerProperties.GetIsInDesignMode(this)) return;
 
-            watch.Start();
             var dispatcher = await GetAsyncDispatcherAsync();
-            watch.Stop();
-            var uiDispatcherElapsed = watch.Elapsed;
-
-            watch.Restart();
-            var panel = new Grid();
-            for (var i = 0; i < 10000; i++)
-            {
-                panel.Children.Add(new Button());
-            }
-            watch.Stop();
-            var newElapsed = watch.Elapsed;
-
-            watch.Restart();
-            panel.Measure(new Size(300, 500));
-            panel.Arrange(new Rect(0, 0, 300, 500));
-            watch.Stop();
-            var layoutElapsed = watch.Elapsed;
-
-            watch.Restart();
-            AddVisualChild(panel);
-            watch.Stop();
-            var visualTreeElapsed = watch.Elapsed;
-
-            watch.Restart();
-            AddLogicalChild(panel);
-            watch.Stop();
-            var logicalTreeElapsed = watch.Elapsed;
-
-            var format = $"线程 {uiDispatcherElapsed}, 创建 {newElapsed}, 布局 {layoutElapsed}, 可视化树 {visualTreeElapsed}, 逻辑树 {logicalTreeElapsed}";
-            Console.WriteLine(format);
+            var view = await dispatcher.InvokeAsync(CreateLoadingView);
         }
 
         protected override Size MeasureOverride(Size availableSize)
