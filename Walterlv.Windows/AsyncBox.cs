310470f045b2caee160c19a51d3db74201db0dee
walterlv <xblero@gmail.com>
2018-09-08 16:15:02 +0800

    Add basic files.

---
+++ Walterlv.Windows/AsyncBox.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Walterlv.Windows
+{
+    public class AsyncBox : FrameworkElement
+    {
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
