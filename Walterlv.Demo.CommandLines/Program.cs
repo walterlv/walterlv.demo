a156217ee84886bfb0c7faba5b66574f51ee0775
walterlv <xblero@gmail.com>
2018-09-11 20:10:13 +0800

    Add project files.

---
+++ Walterlv.Demo.CommandLines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.CommandLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
