f15b011367bf5272b4925806b28af1e6f2264ed9
walterlv <xblero@gmail.com>
2018-09-20 19:47:00 +0800

    Add project files.

---
+++ Walterlv.Demo.Patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
