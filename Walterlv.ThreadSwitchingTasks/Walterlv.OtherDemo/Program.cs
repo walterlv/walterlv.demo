d4d5e0a5f12db003dcc47346b9d06680c7d5819e
walterlv <xblero@gmail.com>
2018-10-01 21:03:13 +0800

    Add thread swithing demo.

---
+++ Walterlv.ThreadSwitchingTasks/Walterlv.OtherDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Walterlv.Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(SynchronizationContext.Current?.ToString() ?? "null");
+            MyControl my = new MyControl();
+            Console.WriteLine(SynchronizationContext.Current?.ToString() ?? "null");
+
+            var task = Task.Run(() => { });
+            await task;
+            Thread.Sleep(-1);
+        }
+
+        public class MyControl : Control
+        {
+        }
+    }
+}
---
+++ Walterlv.ThreadSwitchingTasks/Walterlv.ThreadSwitchingTasks/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Walterlv.ThreadSwitchingTasks
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            var foo = new Foo();
+
+        }
+
+
+    }
+
+    [DebuggerDisplay("{Count,h}")]
+    public class Foo
+    {
+        public int Count = 3;
+    }
+}
