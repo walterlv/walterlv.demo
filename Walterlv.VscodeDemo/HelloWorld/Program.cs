c86d2797f9014a00fee97a8f778b4fa4a8c011c9
walterlv <xblero@gmail.com>
2018-10-04 21:01:56 +0800

    Add vscode demo.

---
+++ Walterlv.VscodeDemo/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("");
+            Task.Delay(5000).Wait();
+            Process.GetProcessById(0).Kill();
+            foreach (var process in Process.GetProcessesByName("easinote"))
+            {
+                try
+                {
+                    process.Kill();
+                }
+                catch (Win32Exception)
+                {
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
