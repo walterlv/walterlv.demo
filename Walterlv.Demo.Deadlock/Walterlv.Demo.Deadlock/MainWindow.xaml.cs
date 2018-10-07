69d88e698f5f59ad46b3922ff3f95b3243d52701
walterlv <xblero@gmail.com>
2018-10-07 21:01:14 +0800

    Add non-git project files.

---
+++ Walterlv.Demo.Deadlock/Walterlv.Demo.Deadlock/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Walterlv.Demo.Deadlock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            UnloadProxyAsync().Wait();
+        }
+
+        private async Task UnloadProxyAsync()
+        {
+            await Task.Run(() => XxxxService.UnloadDriver());
+        }
+
+        private class XxxxService
+        {
+            public static void UnloadDriver()
+            {
+            }
+        }
+    }
+}
---
+++ Walterlv.FastStartup/Walterlv.FastStartup/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Walterlv.FastStartup
+{
+    public class Program
+    {
+        
+    }
+}
