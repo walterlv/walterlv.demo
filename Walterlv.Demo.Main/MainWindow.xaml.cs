b68223603c106addf9e8091caae1f6a8d7487e8a
walterlv <xblero@gmail.com>
2018-10-09 19:25:00 +0800

    Let wpf work.

---
+++ Walterlv.Demo.Main/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Walterlv.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Walterlv.Demo.Main/Program.cs
+++ Walterlv.Demo.Main/Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
-
-namespace Walterlv.Demo
+﻿namespace Walterlv.Demo
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello Walterlv!");
-            Console.Read();
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
         }
     }
 }
