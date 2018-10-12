ae5d10efa88db09ff3cec9f646a242df32939c38
walterlv <xblero@gmail.com>
2018-10-12 21:36:30 +0800

    Add project files.

---
+++ Walterlv.Demo.BindingContext/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.BindingContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
