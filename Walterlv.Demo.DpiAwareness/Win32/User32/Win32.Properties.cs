7b0a36da58442f239ec2210cc538b1618f2c8ce9
walterlv <xblero@gmail.com>
2018-10-23 11:34:24 +0800

    Add dpi p/invoke.

---
+++ Walterlv.Demo.DpiAwareness/Win32/User32/Win32.Properties.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Walterlv.Windows.Native
+{
+    public static partial class Win32
+    {
+        internal static class Properties
+        {
+#if !ANSI
+            public const CharSet BuildCharSet = CharSet.Unicode;
+#else
+            public const CharSet BuildCharSet = CharSet.Ansi;
+#endif
+        }
+    }
+}
