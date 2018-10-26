2d8eb47f119af212f4bf46a3ee258bd78d9bb847
walterlv <xblero@gmail.com>
2018-10-26 20:53:42 +0800

    Add styles temp files.

--- Walterlv.Styles/Walterlv.Styles/App.xaml.cs
+++ Walterlv.Styles/Walterlv.Styles/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Walterlv.Styles
 {
@@ -13,5 +14,13 @@ namespace Walterlv.Styles
     /// </summary>
     public partial class App : Application
     {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            await Task.Delay(2000);
+
+            Resources["Brush.MainButton.Background"] = new SolidColorBrush(Color.FromRgb(0x00, 0x7a, 0xcc));
+        }
     }
 }
