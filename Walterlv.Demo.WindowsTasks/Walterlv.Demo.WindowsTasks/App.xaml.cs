158ec1ccf22edf87376f8d22fd6e7785b47406bd
walterlv <xblero@gmail.com>
2018-10-27 20:52:55 +0800

    Add window task demo files.

--- Walterlv.Demo.WindowsTasks/Walterlv.Demo.WindowsTasks/App.xaml.cs
+++ Walterlv.Demo.WindowsTasks/Walterlv.Demo.WindowsTasks/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Shell;
 
 namespace Walterlv.Demo.WindowsTasks
 {
@@ -13,5 +14,14 @@ namespace Walterlv.Demo.WindowsTasks
     /// </summary>
     public partial class App : Application
     {
+        private void JumpList_OnJumpItemsRejected(object sender, JumpItemsRejectedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void JumpList_OnJumpItemsRemovedByUser(object sender, JumpItemsRemovedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
