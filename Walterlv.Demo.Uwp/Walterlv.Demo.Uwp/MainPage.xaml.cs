f0640da0218ec3d0680cf2ed22a458ada9e84735
walterlv <xblero@gmail.com>
2018-11-19 20:29:31 +0800

    Add uwp temp files.

--- Walterlv.Demo.Uwp/Walterlv.Demo.Uwp/MainPage.xaml.cs
+++ Walterlv.Demo.Uwp/Walterlv.Demo.Uwp/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,18 +15,26 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace Walterlv.Demo.Uwp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DoAsync().Wait();
+            // This line never enters.
+        }
+
+        private async Task DoAsync()
+        {
+            await Task.Delay(1000);
+            //await ApplicationData.Current.LocalFolder.CreateFileAsync($"walterlv",
+            //    CreationCollisionOption.ReplaceExisting);
         }
     }
 }
