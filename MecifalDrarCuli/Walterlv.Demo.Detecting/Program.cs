fdfcacec0653bf5da136ba90a8ce8ae27c667584
walterlv <xblero@gmail.com>
2018-11-22 20:31:22 +0800

    Add MecifalDrarCuli temp files.

---
+++ MecifalDrarCuli/Walterlv.Demo.Detecting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.Detecting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
