d4a6ef423afc7f507fd3167cb51a5f413a8abf19
walterlv <xblero@gmail.com>
2018-11-24 20:32:14 +0800

    Add threding temp files.

--- Walterlv.Demo.Threading/Walterlv.Demo.Threading/Program.cs
+++ Walterlv.Demo.Threading/Walterlv.Demo.Threading/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace Walterlv.Demo.Threading
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Threading Demo by walterlv.com";
+
+            var stopwatch = Stopwatch.StartNew();
+            await Task.Delay(0);
+            var elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Task.Delay(0) : {elapsed}");
+
+            stopwatch.Restart();
+            await Task.Delay(1);
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Task.Delay(1) : {elapsed}");
+
+            stopwatch.Restart();
+            await Task.Delay(15);
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Task.Delay(15): {elapsed}");
+
+            stopwatch.Restart();
+            await Task.Delay(16);
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Task.Delay(16): {elapsed}");
+
+            stopwatch.Restart();
+            await Task.Yield();
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Task.Yield()  : {elapsed}");
+
+            stopwatch.Restart();
+            elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Nothing       : {elapsed}");
+
+            Console.Read();
         }
     }
 }
