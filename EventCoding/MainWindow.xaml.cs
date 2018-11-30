e4f19d74216c09fccda5b3b4e11f0bb4e242a221
walterlv <xblero@gmail.com>
2018-11-30 16:25:53 +0800

    Add basic random code.

--- EventCoding/MainWindow.xaml.cs
+++ EventCoding/MainWindow.xaml.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace EventCoding
+namespace Walterlv.EventCoding
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            var output = await GenerateCodesAsync();
+            this.OutputTextBox.Text = output;
+        }
+
+        private Task<string> GenerateCodesAsync(int length = 4, int count = 200)
+        {
+            return Task.Run(() => GenerateCodes(length, count));
+        }
+
+        private string GenerateCodes(int length = 4, int count = 200)
+        {
+            var random = new Random();
+            var max = (int)Math.Pow(16, length);
+            var codes = new int[count];
+
+            for (var i = 0; i < 200; i++)
+            {
+                var number = random.Next(0, max);
+                while (codes.Contains(number))
+                {
+                    number = random.Next(0, max);
+                }
+                codes[i] = number;
+            }
+
+            return string.Join(Environment.NewLine, codes.Select(x => x.ToString("X").PadLeft(4, '0')));
         }
     }
 }
