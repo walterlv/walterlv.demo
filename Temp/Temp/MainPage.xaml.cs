3c5c6e270934db89bfbca4729b5131a1effab97e
walterlv <xblero@gmail.com>
2018-12-04 20:37:25 +0800

    Add temp files.

--- Temp/Temp/MainPage.xaml.cs
+++ Temp/Temp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,28 @@ namespace Temp
         public MainPage()
         {
             this.InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                TextBlock textBlock = new TextBlock();
+                textBlock.FontSize = 52;
+                //textBlock.Foreground = new SolidColorBrush(Colors.Red);
+                textBlock.Text = "一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n一二三四\\n";
+                Viewbox Vb = new Viewbox();
+                Vb.Projection = new PlaneProjection();
+                //((PlaneProjection)Vb.Projection).CenterOfRotationX = 0.5;
+                //((PlaneProjection)Vb.Projection).CenterOfRotationY = 0.5;
+                ((PlaneProjection)Vb.Projection).RotationY = 35;
+                //((PlaneProjection)Vb.Projection).RotationZ = 360;
+                Vb.Child = textBlock;
+                canvas.Children.Add(Vb);
+                //Canvas.SetLeft(Vb, 1);
+                //Canvas.SetTop(Vb, 1);
+            }
         }
     }
 }
