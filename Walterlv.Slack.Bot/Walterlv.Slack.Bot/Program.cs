c2efa5057576cfdb2d840da401a650a72c9ea673
walterlv <xblero@gmail.com>
2018-12-11 20:40:44 +0800

    Add slack temp files.

--- Walterlv.Slack.Bot/Walterlv.Slack.Bot/Program.cs
+++ Walterlv.Slack.Bot/Walterlv.Slack.Bot/Program.cs
@@ -5,20 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.CodeAnalysis.Operations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Walterlv.Slack.Bot
+namespace Walterlv.NullableDemo
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Console.Title = "walterlv's demo";
+
+            int? value = GetValue(1);
+            object o = value;
+
+            Console.WriteLine($"value is nullable? {IsOfNullableType(value)}");
+            Console.WriteLine($"o     is nullable? {IsOfNullableType(o)}");
+
+            Console.ReadLine();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        private static int? GetValue(int? source) => source;
+
+        static bool IsOfNullableType<T>(T _) => Nullable.GetUnderlyingType(typeof(T)) != null;
     }
 }
