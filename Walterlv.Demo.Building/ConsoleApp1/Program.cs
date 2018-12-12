06f6b05f7621c032ee1b03684141a7f3ed57a896
walterlv <xblero@gmail.com>
2018-12-12 20:43:33 +0800

    Add building temp files.

---
+++ Walterlv.Demo.Building/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            helper();
+            helper();
+            helper();
+            helper();
+            helper();
+
+            Console.ReadLine();
+        }
+
+        static void helper()
+        {
+            Console.WriteLine("this is a test");
+        }
+    }
+}
---
+++ Walterlv.Demo.Building/Walterlv.Demo.Building/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Walterlv.Demo.Building
+{
+    public class Demo : Freezable
+    {
+        public Demo()
+        {
+            
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new Demo();
+        }
+
+        public override string ToString()
+        {
+            return GetHashCode().ToString("D");
+        }
+    }
+}
