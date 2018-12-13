819ad083256ecf02c771b5c4fd3fc838b2508f77
walterlv <xblero@gmail.com>
2018-12-13 16:07:57 +0800

    Add project files.

---
+++ Walterlv.Demo.Tasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
