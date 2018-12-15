c7b75d81eb1c3693f9e8f02f244785f7ee35cbf6
walterlv <xblero@gmail.com>
2018-12-15 17:03:19 +0800

    Add project files.

---
+++ Walterlv.DirectXDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.DirectXDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
