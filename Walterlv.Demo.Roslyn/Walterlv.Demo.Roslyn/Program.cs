17ab2b178b11c70842177fca4797a927cad17171
walterlv <xblero@gmail.com>
2018-12-19 20:44:40 +0800

    Add roslyn temp files.

--- Walterlv.Demo.Roslyn/Walterlv.Demo.Roslyn/Program.cs
+++ Walterlv.Demo.Roslyn/Walterlv.Demo.Roslyn/Program.cs
@@ -4,9 +4,23 @@ namespace Walterlv.Demo.Roslyn
 {
     class Program
     {
-        static void Main(string[] args)
+        static unsafe void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ReferenceEquals(A, A));
+            Console.WriteLine(ReferenceEquals(C, C));
+            Console.WriteLine(ReferenceEquals(E, E));
+            Console.WriteLine(ReferenceEquals(G, G));
+            
+            Console.ReadKey(true);
         }
+
+        private static string A => $"walterlv is a {B}";
+        private static string B => "逗比";
+        private static string C => $"walterlv is a {D}";
+        private static string D = "逗比";
+        private static string E => $"walterlv is a {F}";
+        private static readonly string F = "逗比";
+        private static string G => $"walterlv is a {H}";
+        private const string H = "逗比";
     }
 }
