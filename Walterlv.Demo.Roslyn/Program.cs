daa42be83b92058f2b7d6d58ad5831cc148d8370
walterlv <xblero@gmail.com>
2019-01-16 20:45:57 +0800

    Add project files.

---
+++ Walterlv.Demo.Roslyn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.Roslyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
