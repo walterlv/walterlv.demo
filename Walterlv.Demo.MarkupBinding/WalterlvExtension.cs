7f399d58d461c1207f44db6e9ec0345885487f35
walterlv <xblero@gmail.com>
2019-01-31 23:13:16 +0800

    Add markupextension.

---
+++ Walterlv.Demo.MarkupBinding/WalterlvExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace Walterlv.Demo.MarkupBinding
+{
+    public class WalterlvExtension : MarkupExtension
+    {
+        private object _value;
+
+        public object Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                OnValueChanged(value);
+            }
+        }
+
+        private void OnValueChanged(object newValue)
+        {
+            
+        }
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Value;
+        }
+    }
+}
