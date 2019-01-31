6cccc74b70168ae98341ad854c75c8b1928584eb
walterlv <xblero@gmail.com>
2019-01-31 09:39:28 +0800

    Add project files.

---
+++ Walterlv.Demo.NETCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Walterlv.Demo.NETStandard;
+
+namespace Walterlv.Demo.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            foo.Run();
+        }
+    }
+}
