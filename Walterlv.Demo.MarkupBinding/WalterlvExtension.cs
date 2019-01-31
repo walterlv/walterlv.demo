3601790fa379939836abb3acd3cd4463394485bc
walterlv <xblero@gmail.com>
2019-02-01 00:11:31 +0800

    Add result.

--- Walterlv.Demo.MarkupBinding/WalterlvExtension.cs
+++ Walterlv.Demo.MarkupBinding/WalterlvExtension.cs
@@ -1,31 +1,76 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace Walterlv.Demo.MarkupBinding
 {
     public class WalterlvExtension : MarkupExtension
     {
-        private object _value;
+        private readonly ClrBindingExchanger _valueExchanger;
 
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached(
+            "Value", typeof(object), typeof(WalterlvExtension),
+            new PropertyMetadata(null, ClrBindingExchanger.ValueChangeCallback));
+
+        public WalterlvExtension()
+        {
+            _valueExchanger = new ClrBindingExchanger(this, ValueProperty, OnValueChanged);
+        }
+
+        [Bindable(true)]
         public object Value
         {
-            get => _value;
-            set
-            {
-                _value = value;
-                OnValueChanged(value);
-            }
+            get => _valueExchanger.GetValue();
+            set => _valueExchanger.SetValue(value);
         }
 
-        private void OnValueChanged(object newValue)
+        private void OnValueChanged(object oldValue, object newValue)
         {
-            
-        }
 
+        }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             return Value;
+        }
+    }
+
+    internal class ClrBindingExchanger : DependencyObject
+    {
+        private readonly object _owner;
+        private readonly DependencyProperty _attachedProperty;
+        private readonly Action<object, object> _valueChangeCallback;
+
+        public ClrBindingExchanger(object owner, DependencyProperty attachedProperty,
+            Action<object, object> valueChangeCallback = null)
+        {
+            _owner = owner;
+            _attachedProperty = attachedProperty;
+            _valueChangeCallback = valueChangeCallback;
+        }
+
+        public object GetValue()
+        {
+            return GetValue(_attachedProperty);
+        }
+
+        public void SetValue(object value)
+        {
+            if (value is Binding binding)
+            {
+                BindingOperations.SetBinding(this, _attachedProperty, binding);
+            }
+            else
+            {
+                SetValue(_attachedProperty, value);
+            }
+        }
+
+        public static void ValueChangeCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((ClrBindingExchanger) d)._valueChangeCallback?.Invoke(e.OldValue, e.NewValue);
         }
     }
 }
