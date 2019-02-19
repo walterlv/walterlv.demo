7798a21132efcc0bc7bed3f966ea38ecbdfe5d0e
walterlv <xblero@gmail.com>
2019-02-19 21:31:00 +0800

    Add command line getter.

--- Walterlv.Debugging/Program.cs
+++ Walterlv.Debugging/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
 
 namespace Walterlv.Debugging
 {
@@ -22,7 +24,43 @@ namespace Walterlv.Debugging
                     StartInfo = new ProcessStartInfo(Process.GetCurrentProcess().MainModule.FileName, "--child"),
                 };
                 process.Start();
+                var processArgs = process.GetCommandLineArgs();
+                Console.WriteLine(processArgs);
                 process.WaitForExit();
+            }
+        }
+    }
+
+    public static class ProcessExtensions
+    {
+        public static string GetCommandLineArgs(this Process process)
+        {
+            if (process is null) throw new ArgumentNullException(nameof(process));
+
+            try
+            {
+                return GetCommandLineArgsCore();
+            }
+            catch (Win32Exception ex) when ((uint) ex.ErrorCode == 0x80004005)
+            {
+                // 没有对该进程的安全访问权限。
+                return string.Empty;
+            }
+            catch (InvalidOperationException)
+            {
+                // 进程已退出。
+                return string.Empty;
+            }
+
+            string GetCommandLineArgsCore()
+            {
+                using (var searcher = new ManagementObjectSearcher(
+                    "SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+                using (var objects = searcher.Get())
+                {
+                    var @object = objects.Cast<ManagementBaseObject>().SingleOrDefault();
+                    return @object?["CommandLine"]?.ToString() ?? "";
+                }
             }
         }
     }
