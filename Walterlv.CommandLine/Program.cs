d672245deaf61a18ef73caaf68a8ec00f75ad028
walterlv <xblero@gmail.com>
2019-03-06 21:04:41 +0800

    Add performance func.

--- Walterlv.CommandLine/Program.cs
+++ Walterlv.CommandLine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Walterlv.Framework;
 
 namespace Walterlv
@@ -7,9 +8,17 @@ namespace Walterlv
     {
         static void Main(string[] args)
         {
-            var commandLine = CommandLine.Parse(args, urlProtocol: "walterlv");
-            var option = commandLine.As<Options>();
-
+            var stopwatch = new Stopwatch();
+            for (var i = 0; i < 100; i++)
+            {
+                stopwatch.Restart();
+                var commandLine = CommandLine.Parse(args, urlProtocol: "walterlv");
+                var option = commandLine.As<Options>(new OptionsParser());
+                stopwatch.Stop();
+                Console.Write(option.ToString()[0]);
+                Console.CursorLeft = 0;
+                Console.WriteLine($"{i} - {stopwatch.Elapsed.Ticks} ticks & {stopwatch.Elapsed.TotalMilliseconds} ms");
+            }
             Console.ReadLine();
         }
     }
