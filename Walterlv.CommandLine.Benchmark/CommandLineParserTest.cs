d3fe1d96c5766720cc4788c150e3a55d21904d88
walterlv <xblero@gmail.com>
2019-03-06 21:25:47 +0800

    Add url parser performance test.

--- Walterlv.CommandLine.Benchmark/CommandLineParserTest.cs
+++ Walterlv.CommandLine.Benchmark/CommandLineParserTest.cs
@@ -18,11 +18,37 @@ namespace Walterlv.CommandLine.Benchmark
             "89EA9D26-6464-4E71-BD04-AA6516063D83",
         };
 
-        [Benchmark]
+        private static readonly string[] CommandLineUrl =
+        {
+            @"walterlv://open/?file=C:\Users\lvyi\Desktop\%E9%87%8D%E5%91%BD%E5%90%8D%E8%AF%95%E9%AA%8C.enbx&cloud=true&iwb=true&placement=Outside&startupSession=89EA9D26-6464-4E71-BD04-AA6516063D83",
+        };
+
+        [Benchmark(Baseline = true)]
         public void ParseAsOptions()
         {
             var commandLine = Framework.CommandLine.Parse(CommandLineArguments, urlProtocol: "walterlv");
             commandLine.As<Options>(new OptionsParser());
+        }
+
+        [Benchmark]
+        public void ParseAsAutoOptions()
+        {
+            var commandLine = Framework.CommandLine.Parse(CommandLineArguments, urlProtocol: "walterlv");
+            commandLine.As<Options>();
+        }
+
+        [Benchmark]
+        public void ParseUrlAsOptions()
+        {
+            var commandLine = Framework.CommandLine.Parse(CommandLineUrl, urlProtocol: "walterlv");
+            commandLine.As<Options>(new OptionsParser());
+        }
+
+        [Benchmark]
+        public void ParseUrlAsAutoOptions()
+        {
+            var commandLine = Framework.CommandLine.Parse(CommandLineUrl, urlProtocol: "walterlv");
+            commandLine.As<Options>();
         }
     }
 }
