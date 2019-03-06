477c2dc97a31241e9d3a79817a05e9b46c7c7480
walterlv <xblero@gmail.com>
2019-03-06 21:16:32 +0800

    Add benchmark test project.

---
+++ Walterlv.CommandLine.Benchmark/CommandLineParserTest.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Walterlv.CommandLine.Benchmark
+{
+    public class CommandLineParserTest
+    {
+        private static readonly string[] CommandLineArguments =
+        {
+            @"C:\Users\lvyi\Desktop\重命名试验.enbx",
+            "-Cloud",
+            "-Iwb",
+            "-m",
+            "Display",
+            "-s",
+            "-p",
+            "Outside",
+            "-StartupSession",
+            "89EA9D26-6464-4E71-BD04-AA6516063D83",
+        };
+
+        [Benchmark]
+        public void ParseAsOptions()
+        {
+            var commandLine = Framework.CommandLine.Parse(CommandLineArguments, urlProtocol: "walterlv");
+            commandLine.As<Options>(new OptionsParser());
+        }
+    }
+}
