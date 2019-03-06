ed806a6a6bc512fdb569608644fe9138cd5bb5db
walterlv <xblero@gmail.com>
2019-03-06 22:15:43 +0800

    Add linux style benchmark test.

--- Walterlv.CommandLine.Benchmark/CommandLineParserTest.cs
+++ Walterlv.CommandLine.Benchmark/CommandLineParserTest.cs
@@ -4,7 +4,7 @@ namespace Walterlv.CommandLine.Benchmark
 {
     public class CommandLineParserTest
     {
-        private static readonly string[] CommandLineArguments =
+        private static readonly string[] WindowsStyleArgs =
         {
             @"C:\Users\lvyi\Desktop\重命名试验.enbx",
             "-Cloud",
@@ -18,36 +18,64 @@ namespace Walterlv.CommandLine.Benchmark
             "89EA9D26-6464-4E71-BD04-AA6516063D83",
         };
 
-        private static readonly string[] CommandLineUrl =
+        private static readonly string[] LinuxStyleArgs =
+        {
+            @"C:\Users\lvyi\Desktop\重命名试验.enbx",
+            "--cloud",
+            "--iwb",
+            "-m",
+            "Display",
+            "-s",
+            "-p",
+            "Outside",
+            "--startup-session",
+            "89EA9D26-6464-4E71-BD04-AA6516063D83",
+        };
+
+        private static readonly string[] UrlArgs =
         {
             @"walterlv://open/?file=C:\Users\lvyi\Desktop\%E9%87%8D%E5%91%BD%E5%90%8D%E8%AF%95%E9%AA%8C.enbx&cloud=true&iwb=true&silence=true&placement=Outside&startupSession=89EA9D26-6464-4E71-BD04-AA6516063D83",
         };
 
         [Benchmark(Baseline = true)]
-        public void ParseAsOptions()
+        public void ParseWindows()
         {
-            var commandLine = Framework.CommandLine.Parse(CommandLineArguments, urlProtocol: "walterlv");
+            var commandLine = Framework.CommandLine.Parse(WindowsStyleArgs, urlProtocol: "walterlv");
             commandLine.As<Options>(new OptionsParser());
         }
 
         [Benchmark]
-        public void ParseAsAutoOptions()
+        public void ParseWindowsAuto()
         {
-            var commandLine = Framework.CommandLine.Parse(CommandLineArguments, urlProtocol: "walterlv");
+            var commandLine = Framework.CommandLine.Parse(WindowsStyleArgs, urlProtocol: "walterlv");
             commandLine.As<Options>();
         }
 
         [Benchmark]
-        public void ParseUrlAsOptions()
+        public void ParseLinux()
         {
-            var commandLine = Framework.CommandLine.Parse(CommandLineUrl, urlProtocol: "walterlv");
+            var commandLine = Framework.CommandLine.Parse(LinuxStyleArgs, urlProtocol: "walterlv");
             commandLine.As<Options>(new OptionsParser());
         }
 
         [Benchmark]
-        public void ParseUrlAsAutoOptions()
+        public void ParseLinuxAuto()
         {
-            var commandLine = Framework.CommandLine.Parse(CommandLineUrl, urlProtocol: "walterlv");
+            var commandLine = Framework.CommandLine.Parse(LinuxStyleArgs, urlProtocol: "walterlv");
+            commandLine.As<Options>();
+        }
+
+        [Benchmark]
+        public void ParseUrl()
+        {
+            var commandLine = Framework.CommandLine.Parse(UrlArgs, urlProtocol: "walterlv");
+            commandLine.As<Options>(new OptionsParser());
+        }
+
+        [Benchmark]
+        public void ParseUrlAuto()
+        {
+            var commandLine = Framework.CommandLine.Parse(UrlArgs, urlProtocol: "walterlv");
             commandLine.As<Options>();
         }
     }
