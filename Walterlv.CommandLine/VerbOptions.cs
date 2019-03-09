e0d2a8c53fa9eab156159e7b35260856b3c78512
walterlv <xblero@gmail.com>
2019-03-10 02:24:28 +0800

    添加谓词参数类型。

---
+++ Walterlv.CommandLine/VerbOptions.cs
@@ -0,0 +1,23 @@
+﻿using Cvte.Cli;
+
+namespace Walterlv
+{
+    [Verb("Edit")]
+    public class EditOptions
+    {
+        [Value(0), Option('f', "File")] public string FilePath { get; set; }
+    }
+
+    [Verb("Print")]
+    public class PrintOptions
+    {
+        [Value(0), Option('f', "File")] public string FilePath { get; set; }
+        [Option('p', "Printer")] public string Printer { get; set; }
+    }
+
+    [Verb("Share")]
+    public class ShareOptions
+    {
+        [Option('t', "Target")] public string Target { get; set; }
+    }
+}
