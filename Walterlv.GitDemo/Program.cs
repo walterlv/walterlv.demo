851072750c4805ae24c40934621fd33438ef74ad
walterlv <xblero@gmail.com>
2019-03-26 19:39:21 +0800

    添加项目文件。

---
+++ Walterlv.GitDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Walterlv.GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+        }
+
+        private static string RunCommand(string executablePath, string arguments,
+            string workingDirectory)
+        {
+            var info = new ProcessStartInfo("git", "status")
+            {
+                CreateNoWindow = false,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                WorkingDirectory = workingDirectory,
+            };
+            var builder = new StringBuilder();
+            var process = new Process
+            {
+                StartInfo = info,
+            };
+            process.OutputDataReceived += (sender, args) => builder.Append(args.Data);
+            process.Start();
+            process.WaitForExit();
+            return builder.ToString();
+        }
+    }
+}
