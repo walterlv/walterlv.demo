8307bc2645b6136cf0e5b43cc40fa299fd17fbe3
walterlv <xblero@gmail.com>
2019-04-05 20:03:30 +0800

    Add temp files.

---
+++ TroukawDerjalyem/TroukawDerjalyem/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Walterlv.Demo
+{
+    public class Class1
+    {
+        private bool _done;
+        private Action<string> _eee;
+
+        public void Do1()
+        {
+            if (!_done)
+            {
+                _done = true;
+                DoCore0("");
+            }
+
+            _eee -= DoCore0;
+            _eee += DoCore0;
+
+            void DoCore0(string text)
+            {
+                Console.WriteLine(text);
+            }
+        }
+
+        public void Do2()
+        {
+            if (!_done)
+            {
+                _done = true;
+                DoCore("");
+            }
+        }
+
+        private void DoCore(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
