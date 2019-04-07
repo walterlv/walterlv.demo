5d4e2d7c2ed9141a22509b27012c9d5db0d812ca
walterlv <xblero@gmail.com>
2019-04-07 20:05:50 +0800

    Add high cpu files.

--- HighCPU/HighCPU/MainWindow.xaml.cs
+++ HighCPU/HighCPU/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Configuration;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace HighCPU
 {
@@ -20,9 +11,33 @@ namespace HighCPU
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _threadNum = 2;
+        private int _threadSleepTime = 1000;
+        private int num = 1;
+
         public MainWindow()
         {
             InitializeComponent();
+            _threadNum = Convert.ToInt32(ConfigurationManager.AppSettings["ThreadNum"]);
+            _threadSleepTime = Convert.ToInt32(ConfigurationManager.AppSettings["ThreadSleepTime"]);
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Block.Text = $"已经运行{_threadNum * num++}个线程";
+            for (int i = 0; i < _threadNum; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    var num = i;
+                    while (true)
+                    {
+                        Console.WriteLine($"{num}");
+                        Thread.Sleep(_threadSleepTime);
+                    }
+                });
+                thread.Start();
+            }
         }
     }
 }
