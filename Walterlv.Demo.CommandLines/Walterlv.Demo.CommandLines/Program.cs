98244762f6d7395544143035d7d32d0644413dc5
walterlv <xblero@gmail.com>
2019-04-14 20:21:08 +0800

    Add command line temp files.

--- Walterlv.Demo.CommandLines/Walterlv.Demo.CommandLines/Program.cs
+++ Walterlv.Demo.CommandLines/Walterlv.Demo.CommandLines/Program.cs
@@ -7,17 +7,25 @@ namespace Walterlv.Demo.CommandLines
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"参数总数：{args.Length}");
+            Console.WriteLine($"Main 函数参数列表中参数总数：{args.Length}");
+            OutputArgsInfo(args);
 
+            args = Environment.GetCommandLineArgs();
+            Console.WriteLine($"GetCommandLineArgs 参数总数：{args.Length}");
+            OutputArgsInfo(args);
+
+            Console.WriteLine($"按任意键继续……");
+            Console.ReadKey();
+        }
+
+        private static void OutputArgsInfo(string[] args)
+        {
             var digitCount = (args.Length - 1).ToString(CultureInfo.InvariantCulture).Length;
 
             for (var i = 0; i < args.Length; i++)
             {
                 Console.WriteLine($"[{i.ToString().PadLeft(digitCount, ' ')}] {args[i]}");
             }
-
-            Console.WriteLine($"按任意键继续……");
-            Console.ReadKey();
         }
     }
 }
