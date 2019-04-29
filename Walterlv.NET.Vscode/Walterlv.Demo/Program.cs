b97ca6343a503f91cbadf4cde4781b15c70646fe
walterlv <xblero@gmail.com>
2019-04-29 21:06:53 +0800

    Add vscode for .net.

---
+++ Walterlv.NET.Vscode/Walterlv.Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Walterlv.Library;
+
+namespace Walterlv.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.PrintDouB();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Walterlv.NET.Vscode/Walterlv.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Library
+{
+    public class Class1
+    {
+        public static void PrintDouB()
+        {
+            System.Console.WriteLine("walterlv is a 逗比");
+        }
+    }
+}
