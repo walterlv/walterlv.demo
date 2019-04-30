4dfd5506ff84581a66b73f23551d21b27f27b509
walterlv <xblero@gmail.com>
2019-04-30 14:55:58 +0800

    Add get mouse position.

---
+++ Walterlv.RevealEffects.Win32Exception/Walterlv.RevealEffects.Win32Exception/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Walterlv.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += OnRendering; 
+        }
+
+        private void OnRendering(object sender, EventArgs e)
+        {
+            DebugTextBlock.Text = Mouse.GetPosition(DebugTextBlock).ToString();
+            DebugButton.Content = Mouse.GetPosition(DebugButton).ToString();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
