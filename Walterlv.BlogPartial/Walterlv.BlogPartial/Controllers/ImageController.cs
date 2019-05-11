b441cd14db5ca7f0f0c87fa919d3abc98e3f44e2
walterlv <xblero@gmail.com>
2019-05-11 15:20:18 +0800

    Add blog partial.

---
+++ Walterlv.BlogPartial/Walterlv.BlogPartial/Controllers/ImageController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+namespace Walterlv.BlogPartial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private IMemoryCache _cache;
+
+        /// <inheritdoc />
+        public ImageController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        [Route("bulletin.png")]
+        [HttpGet]
+        public FileResult GetImage()
+        {
+            Count++;
+
+            StringBuilder str = new StringBuilder();
+            str.Append(DateTime.Now);
+            str.Append(" ");
+            str.Append("用户访问 ");
+
+            if (TryGetUserIpFromFrp(HttpContext.Request, out var ip))
+            {
+                str.Append("用户Ip=");
+                str.Append(ip);
+                str.Append(" ");
+            }
+
+            str.Append($"总共有{Count}访问");
+
+            if (HttpContext.Request.Headers.TryGetValue("User-Agent", out var userAgent))
+            {
+                str.Append("\r\n");
+                str.Append("当前用户浏览器");
+                str.Append(userAgent);
+            }
+
+            Console.WriteLine(str);
+
+            var file = _cache.GetOrCreate("Image", entry => System.IO.File.ReadAllBytes("bulletin.png"));
+
+            return File(file, "image/png");
+        }
+
+        private static bool TryGetUserIpFromFrp(HttpRequest httpContextRequest, out StringValues ip)
+        {
+            return httpContextRequest.Headers.TryGetValue("X-Forwarded-For", out ip);
+        }
+
+        private static int Count { set; get; }
+
+
+        [Route("UrlMove"), HttpGet]
+        public IActionResult UrlMove()
+        {
+            return Redirect("https://blog.walterlv.com");
+        }
+    }
+}
---
+++ Walterlv.Demo.WpfDirtyRegion/Walterlv.Demo.WpfDirtyRegion/DirtyElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Walterlv.Demo.WpfDirtyRegion
+{
+    public class DirtyElement : FrameworkElement
+    {
+        private readonly List<Visual> _children = new List<Visual>();
+
+        public DirtyElement()
+        {
+            for (var i = 0; i < 1; i++)
+            {
+                var child = new DrawingVisual();
+                _children.Add(child);
+                AddVisualChild(child);
+            }
+
+            for (var i = 0; i < 1; i++)
+            {
+                var child = new DrawingVisual();
+                _children.Add(child);
+                AddVisualChild(child);
+            }
+        }
+
+        protected override int VisualChildrenCount => _children.Count;
+        protected override Visual GetVisualChild(int index) => _children[index];
+
+        public void Draw()
+        {
+            var i = 0;
+            foreach (var child in _children.OfType<DrawingVisual>())
+            {
+                using (var dc = child.RenderOpen())
+                {
+                    var rect = i++ < _children.Count / 2 ? new Rect(200, 0, 2, 400) : new Rect(0, 200, 400, 2);
+                    var brush = DateTime.Now.Second % 2 == 0 ? Brushes.Teal : Brushes.Brown;
+                    dc.DrawRectangle(brush, null, rect);
+                }
+            }
+        }
+    }
+
+    public class DirtyVisual : Visual
+    {
+        private readonly bool _direction;
+
+        public DirtyVisual(bool direction)
+        {
+            _direction = direction;
+        }
+    }
+}
