be9f7b8d6dfd29c0ca138cda189d08e506c799ca
walterlv <xblero@gmail.com>
2019-05-13 07:43:31 +0800

    Add touch demo.

---
+++ Walterlv.Demo.TouchSimulation/Walterlv.Demo.TouchSimulation/FakeTouchDevice.cs
@@ -0,0 +1,91 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Walterlv.Demo.TouchSimulation
+{
+    public class FakeTouchDevice : TouchDevice
+    {
+        /// <inheritdoc />
+        public FakeTouchDevice(int deviceId, Window window) : base(deviceId)
+        {
+            Window = window;
+        }
+
+        /// <summary>
+        /// 触摸点
+        /// </summary>
+        public Point Position { set; get; }
+
+        /// <summary>
+        /// 触摸大小
+        /// </summary>
+        public Size Size { set; get; }
+
+        public void Down()
+        {
+            TouchAction = TouchAction.Down;
+
+            if (!IsActive)
+            {
+                SetActiveSource(PresentationSource.FromVisual(Window));
+
+                Activate();
+                ReportDown();
+            }
+            else
+            {
+                ReportDown();
+            }
+        }
+
+        public void Move()
+        {
+            TouchAction = TouchAction.Move;
+
+            ReportMove();
+        }
+
+        public void Up()
+        {
+            TouchAction = TouchAction.Up;
+
+            ReportUp();
+            Deactivate();
+        }
+
+
+        private Window Window { get; }
+
+        private TouchAction TouchAction { set; get; }
+
+        /// <inheritdoc />
+        public override TouchPoint GetTouchPoint(IInputElement relativeTo)
+        {
+            return new TouchPoint(this, Position, new Rect(Position, Size), TouchAction);
+        }
+
+        /// <inheritdoc />
+        public override TouchPointCollection GetIntermediateTouchPoints(IInputElement relativeTo)
+        {
+            return new TouchPointCollection()
+            {
+                GetTouchPoint(relativeTo)
+            };
+        }
+    }
+
+    //public class DemoTouchDevice : TouchDevice
+    //{
+    //    public DemoTouchDevice(int deviceId) : base(deviceId)
+    //    {
+    //    }
+
+    //    public override TouchPoint GetTouchPoint(IInputElement relativeTo)
+    //    {
+    //    }
+
+    //    public override TouchPointCollection GetIntermediateTouchPoints(IInputElement relativeTo)
+    //    {
+    //    }
+    //}
+}
---
+++ Walterlv.Demo.TouchSimulation/Walterlv.Demo.TouchSimulation/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Walterlv.Demo.TouchSimulation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StylusDown += OnStylusDown;
+            TouchDown += OnTouchDown;
+            MouseDown += OnMouseDown;
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var device = new FakeTouchDevice(1, this);
+            while (true)
+            {
+                await Task.Delay(1000);
+                device.Down();
+            }
+        }
+
+        private void OnTouchDown(object sender, TouchEventArgs e)
+        {
+            Console.WriteLine(e.TouchDevice);
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine(e.StylusDevice);
+        }
+
+        private void OnStylusDown(object sender, StylusDownEventArgs e)
+        {
+            Console.WriteLine(e.StylusDevice);
+        }
+    }
+}
