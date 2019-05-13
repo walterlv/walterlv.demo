fd010e02614b1ed5f96fc99a93cee90ea2199662
walterlv <xblero@gmail.com>
2019-05-13 12:08:56 +0800

    Add xaml bug demo.

---
+++ XamlBugs.StaticProperties/XamlBugs.StaticProperties/Resources.cs
@@ -0,0 +1,12 @@
+﻿// This namespace can be changed to any other and the bug still reproduces.
+namespace XamlBugs.StaticProperties
+{
+    /// <summary>
+    /// The class that provide a `Border` property.
+    /// You can change this class name to any other and the bug still reproduces.
+    /// </summary>
+    public static class FooClass
+    {
+        public static string Border => "";
+    }
+}
