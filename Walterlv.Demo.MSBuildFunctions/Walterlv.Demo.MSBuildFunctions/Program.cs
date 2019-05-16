bc0e4f4800efee3720a60b32acd6e47e31227b34
walterlv <xblero@gmail.com>
2019-05-16 14:14:18 +0800

    Add msbuild functions.

---
+++ Walterlv.Demo.MSBuildFunctions/Walterlv.Demo.MSBuildFunctions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Walterlv.Demo.MSBuildFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable("TEMP");
+            var newTempFolder = @"C:\Walterlv\ApplicationTemp";
+            Environment.SetEnvironmentVariable("TEMP", newTempFolder);
+            Environment.SetEnvironmentVariable("TMP", newTempFolder);
+            var environmentVariable2 = Environment.GetEnvironmentVariable("TEMP");
+            var tempPath = Path.GetTempFileName();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
