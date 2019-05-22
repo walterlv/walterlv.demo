374e7ea057296fc8c2c46f61a63ba1edddac5cec
walterlv <xblero@gmail.com>
2019-05-22 19:17:28 +0800

    Add process exited demo.

---
+++ Walterlv.Demo.AliveProcesses/Walterlv.Demo.AliveProcesses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Walterlv.Demo.AliveProcesses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var process in Process.GetProcesses())
+            {
+                try
+                {
+                    if (process.HasExited)
+                    {
+                        Console.WriteLine($"{process.ProcessName} - HasExited");
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
