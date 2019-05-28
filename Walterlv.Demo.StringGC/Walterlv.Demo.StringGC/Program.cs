375199abce0395284a87b015321f8e01122fe5d5
walterlv <xblero@gmail.com>
2019-05-28 21:17:23 +0800

    Add gc strings demo.

---
+++ Walterlv.Demo.StringGC/Walterlv.Demo.StringGC/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly: CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
+
+namespace Walterlv.Demo.StringGC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var table = new ConditionalWeakTable<string, Foo>
+            {
+                {"walterlv", new Foo("吕毅")},
+                {"lindexi", new Foo("林德熙")},
+            };
+            var time = DateTime.Now.ToString("T");
+            table.Add(time, new Foo("时间"));
+            time = null;
+
+            Console.WriteLine($"开始个数：{table.Count()}");
+            GC.Collect();
+            Console.WriteLine($"剩余个数：{table.Count()}");
+
+            var walterlv = string.Intern("walterlv");
+            var foo = string.Intern("foo");
+            var time1 = DateTime.Now.ToString("T");
+            var time2 = string.Intern(time1);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Foo
+    {
+        public string Value { get; }
+        public Foo(string value) => Value = value;
+    }
+}
