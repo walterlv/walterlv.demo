c0a216beac02e53da66ca8485161f0a64b80705e
walterlv <xblero@gmail.com>
2019-05-28 09:37:17 +0800

    Add start time test.

---
+++ Walterlv.Demo.StartingElapse/Walterlv.Demo.StartingElapse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Walterlv.Demo.StartingElapse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var time1 = DateTime.Now;
+            var startTime = Process.GetCurrentProcess().StartTime;
+            var time2 = DateTime.Now;
+            stopwatch.Stop();
+            Console.WriteLine($"{time1} - {time2} - {startTime}");
+            Console.ReadLine();
+        }
+    }
+}
