74960bcfe2a211d06b197974aa72bad15ab3f262
walterlv <xblero@gmail.com>
2019-05-31 08:09:29 +0800

    Add loh demo.

---
+++ Walterlv.Demo.LOH/Walterlv.Demo.LOH/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Walterlv.Demo.LOH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new byte[1024 * 1024 * 1024];
+            Console.WriteLine(x);
+            for (var i = 0; i < 10; i++)
+            {
+                x = new byte[1024 * 1024 * 1024];
+                Console.WriteLine(x);
+            }
+            GC.Collect();
+            GC.WaitForFullGCComplete();
+            Console.ReadLine();
+        }
+    }
+}
