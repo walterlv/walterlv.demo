9d9676fb527ae3684f7c4273bb373963f1d1ca1f
walterlv <xblero@gmail.com>
2019-05-31 20:18:37 +0800

    Add empty filter project.

---
+++ Walterlv.Demo.Win32DialogFilters/Walterlv.Demo.Win32DialogFilters/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+
+namespace Walterlv.Demo.Win32DialogFilters
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "图片 (*.png, *.jpg)|*.png;*.jpg|文本 (*.txt)|*.txt|walterlv 的自定义格式 (*.lvyi)|*.lvyi";
+            dialog.ShowDialog(this);
+        }
+    }
+}
