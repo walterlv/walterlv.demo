69eecdf94acb781c0f2849d4d329c28a40d27812
walterlv <xblero@gmail.com>
2019-06-10 16:11:36 +0800

    Add dragon fruit demo.

---
+++ Walterlv.Demo.DragonFruit/Walterlv.Demo.DragonFruit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Walterlv.Demo.DragonFruit
+{
+    class Program
+    {
+        static void Main(string package, string version)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
