e98cd8840e6cc151dfb910eebd10939e0c5ea80d
walterlv <xblero@gmail.com>
2019-06-12 09:49:25 +0800

    Add load assembly fail demo.

---
+++ Walterlv.Demo.AssemblyLoading/Walterlv.Demo.AssemblyLoading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Walterlv.Demo.AssemblyLoading
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await LoadDependencyAssembliesAsync();
+            await RunAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task RunAsync()
+        {
+            try
+            {
+                await ThrowAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Demystify());
+            }
+
+            async Task ThrowAsync() => throw new InvalidOperationException();
+        }
+
+        private static async Task LoadDependencyAssembliesAsync()
+        {
+            var folder = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dependencies");
+            Assembly.LoadFile(Path.Combine(folder, "Ben.Demystifier.dll"));
+            Assembly.LoadFile(Path.Combine(folder, "System.Collections.Immutable.dll"));
+            Assembly.LoadFile(Path.Combine(folder, "System.Reflection.Metadata.dll"));
+        }
+    }
+}
