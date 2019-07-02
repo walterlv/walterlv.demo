2f798f5ec79bec035b5e3f8807402c36460928f4
walterlv <xblero@gmail.com>
2019-07-02 14:49:06 +0800

    Add catch when crash.

---
+++ Walterlv.Demo.CatchWhenCrash/Walterlv.Demo.CatchWhenCrash/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Walterlv.Demo.CatchWhenCrash
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    Console.WriteLine("Try");
+                    throw new FileNotFoundException();
+                }
+                catch (FileNotFoundException ex) when (ex.FileName.EndsWith(".png"))
+                {
+                    Console.WriteLine("Catch 1");
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("Catch 2");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch 3");
+            }
+            Console.WriteLine("End");
+        }
+    }
+}
