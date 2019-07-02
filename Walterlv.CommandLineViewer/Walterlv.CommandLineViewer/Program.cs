5a1cd85707b969aecd1e708de0514f24293f5955
walterlv <xblero@gmail.com>
2019-07-02 14:46:17 +0800

    Add command line viewer.

---
+++ Walterlv.CommandLineViewer/Walterlv.CommandLineViewer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Walterlv.CommandLineViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 输出命令行。
+            var argsLine = string.Join(" ", args);
+            Console.Write(argsLine);
+
+            // 将命令行转发给原程序。
+            var exe = Process.GetCurrentProcess().MainModule.FileName;
+            var originalExe = $"{Path.GetFileNameWithoutExtension(exe)}.origin{Path.GetExtension(exe)}";
+            Process.Start(originalExe, argsLine);
+            Console.ReadLine();
+        }
+    }
+}
