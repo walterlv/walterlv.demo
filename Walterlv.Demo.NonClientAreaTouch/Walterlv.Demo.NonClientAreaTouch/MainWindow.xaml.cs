7a56835e89facbb3396db60febaf9c55274be1c7
walterlv <xblero@gmail.com>
2019-07-04 09:13:55 +0800

    Add demo non client area touch.

---
+++ Walterlv.Demo.NonClientAreaTouch/Walterlv.Demo.NonClientAreaTouch/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Walterlv.Demo.NonClientAreaTouch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_TouchDown(object sender, TouchEventArgs e)
+        {
+            Debug.WriteLine("TouchDown");
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Debug.WriteLine("MouseDown");
+        }
+    }
+}
