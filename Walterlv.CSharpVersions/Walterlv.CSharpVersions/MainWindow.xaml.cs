bff6a970613894e48e8e1f81abd5622e38e3a560
walterlv <xblero@gmail.com>
2019-07-05 14:52:30 +0800

    Test C# new compile.

---
+++ Walterlv.CSharpVersions/Walterlv.CSharpVersions/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Walterlv.CSharpVersions
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool Test()
+        {
+            var a = (1.0, 1.0);
+            var b = (1.0, 1.0);
+            return a == b;
+        }
+    }
+}
