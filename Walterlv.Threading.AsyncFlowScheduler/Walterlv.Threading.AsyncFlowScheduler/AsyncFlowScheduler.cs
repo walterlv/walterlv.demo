e1fd7985ee018654643ca33a97205c9de444014c
walterlv <xblero@gmail.com>
2019-07-07 21:08:25 +0800

    Add async flow.

---
+++ Walterlv.Threading.AsyncFlowScheduler/Walterlv.Threading.AsyncFlowScheduler/AsyncFlowScheduler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Walterlv.Threading
+{
+    public abstract class AsyncFlowScheduler<T>
+    {
+        protected Func<Task<T>> 
+
+        public Task<T> RunAsync(CancellationToken cancellationToken = null)
+        {
+            Task.Run()
+        }
+    }
+
+    public sealed class ConcurrentAsyncFlowScheduler<T> : AsyncFlowScheduler<T>
+    {
+
+    }
+
+    public sealed class FirstAndLastAsyncFlowScheduler<T> : AsyncFlowScheduler<T>
+    {
+
+    }
+
+    public sealed class AnyCompletedAsyncFlowScheduler<T> : AsyncFlowScheduler<T>
+    {
+        
+    }
+
+    public sealed class QueueAsyncFlowScheduler<T> : AsyncFlowScheduler<T>
+    {
+
+    }
+
+    public sealed class DisableWhenRunningFlowScheduler<T> : AsyncFlowScheduler<T>
+    {
+
+    }
+}
---
+++ Walterlv.Threading.AsyncFlowScheduler/Walterlv.Threading.AsyncFlowScheduler/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Walterlv.Threading
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var scheduler = new ConcurrentAsyncFlowScheduler();
+        }
+    }
+}
