c486e046e58d1c998f8ff7d2a61f0e2c4f154edf
walterlv <xblero@gmail.com>
2019-07-24 10:43:43 +0800

    Add analyzers.

--- Walterlv.Demo.Analyzers/Walterlv.Demo.Analyzers/Walterlv.Demo.Analyzers/WalterlvDemoAnalyzersAnalyzer.cs
+++ Walterlv.Demo.Analyzers/Walterlv.Demo.Analyzers/Walterlv.Demo.Analyzers/WalterlvDemoAnalyzersAnalyzer.cs
@@ -13,6 +13,11 @@ namespace Walterlv.Demo.Analyzers
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class WalterlvDemoAnalyzersAnalyzer : DiagnosticAnalyzer
     {
+        public WalterlvDemoAnalyzersAnalyzer()
+        {
+            var a = DiagnosticId;
+        }
+
         public const string DiagnosticId = "WAL001";
 
         private static readonly LocalizableString _title = "×Ô¶¯ÊôÐÔ";
@@ -21,7 +26,7 @@ namespace Walterlv.Demo.Analyzers
         private const string _category = "Usage";
 
         private static readonly DiagnosticDescriptor _rule = new DiagnosticDescriptor(
-            DiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Info,
+            DiagnosticId, _title, _messageFormat, _category, DiagnosticSeverity.Error,
             isEnabledByDefault: true, description: _description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_rule);
