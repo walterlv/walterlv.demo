b4f0b43b97c733d15f979eb225e5411e875e1b33
walterlv <xblero@gmail.com>
2019-07-24 10:43:11 +0800

    Add window chrome demo

---
+++ Walterlv.WindowChromeDemo/Walterlv.WindowChromeDemo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace Walterlv.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                VisibleOr.Visibility = Visibility.Collapsed;
+                await Task.Delay(1000);
+                VisibleOr.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
