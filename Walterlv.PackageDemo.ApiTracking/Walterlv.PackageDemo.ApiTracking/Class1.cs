48fcc42a012b546110ad899e3b4c453ced8e43a8
walterlv <xblero@gmail.com>
2019-07-27 16:54:58 +0800

    Add api tracking.

---
+++ Walterlv.PackageDemo.ApiTracking/Walterlv.PackageDemo.ApiTracking/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Walterlv.PackageDemo.ApiTracking
+{
+    public class Class1
+    {
+        //private static readonly string Foo;
+
+        public string Foo2 { get; } = "欢迎访问我的博客：blog.walterlv.com";
+    }
+}
