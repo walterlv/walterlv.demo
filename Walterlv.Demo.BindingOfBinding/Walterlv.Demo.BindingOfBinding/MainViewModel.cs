d538766f3dc9d1c70d51638f3cc1de653d84d76c
walterlv <xblero@gmail.com>
2019-08-11 14:43:41 +0800

    Add binding demo.

---
+++ Walterlv.Demo.BindingOfBinding/Walterlv.Demo.BindingOfBinding/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Walterlv.Demo.BindingOfBinding
+{
+    public class MainViewModel
+    {
+        public object Instance { get; set; }
+
+        public IList<object> PropertyNames { get; set; }
+    }
+}
---
+++ Walterlv.Demo.BindingOfBinding/Walterlv.Demo.BindingOfBinding/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Cvte.Windows.Threading;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Walterlv.Demo.BindingOfBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
