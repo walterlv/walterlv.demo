81df9229b738af52da70907fc5d6d3b16d42279d
walterlv <xblero@gmail.com>
2019-08-13 14:44:48 +0800

    Add localization demo.

---
+++ Walterlv.UILocalization/Walterlv.UILocalization/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Walterlv.UILocalization
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
