b6b7d3d73cb1130c7768cf107e2410b6ac120b45
walterlv <xblero@gmail.com>
2019-08-31 15:55:29 +0800

    Add close window demo.

---
+++ Walterlv.Exceptions.CloseWindow/Walterlv.Exceptions.CloseWindow/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Walterlv.Exceptions.CloseWindow
+{
+    /// <summary>
+    /// Window.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += OnClosing;
+            Closed += OnClosed;
+        }
+
+        private async void OnClosing(object sender, CancelEventArgs e)
+        {
+            //await Task.Yield();
+            //var point = PointFromScreen(new Point());
+            //Debug.WriteLine(point);
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Walterlv.Exceptions.CloseWindow/Walterlv.Exceptions.CloseWindow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Walterlv.Exceptions.CloseWindow
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new Application
+            {
+                ShutdownMode = ShutdownMode.OnExplicitShutdown,
+            };
+            app.Startup += App_Startup;
+            app.Run();
+        }
+
+        private static async void App_Startup(object sender, StartupEventArgs e)
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var window = new MainWindow
+                {
+                    Left = 3000,
+                };
+                window.Show();
+                await Task.Yield();
+                window.WindowState = WindowState.Maximized;
+                await Task.Delay(1000);
+                window.Close();
+            }
+        }
+    }
+}
