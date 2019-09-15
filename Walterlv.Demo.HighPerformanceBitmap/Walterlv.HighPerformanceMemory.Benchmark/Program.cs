40471726da86c23f3aade37f3e91a7de426a9d4d
walterlv <xblero@gmail.com>
2019-09-15 09:41:15 +0800

    Run writeablebitmap benchmark.

---
+++ Walterlv.Demo.HighPerformanceBitmap/Walterlv.HighPerformanceMemory.Benchmark/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace Walterlv.HighPerformanceMemory
+{
+    class Program
+    {
+        public static Window CurrentWindow { get; set; }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("基准测试开始");
+            BenchmarkRunner.Run<MemoryCopyTests>();
+            Console.WriteLine("基准测试结束");
+        }
+    }
+}
