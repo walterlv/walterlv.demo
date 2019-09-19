f259d51ab44d5e63ebc2f7bd909f8bd974732625
walterlv <xblero@gmail.com>
2019-09-19 08:47:39 +0800

    Add demo program

--- Walterlv.Demo.FileWatcherRecycle/Walterlv.Demo.FileWatcherRecycle/FileSystemWatcherWeakEventRelay.cs
+++ Walterlv.Demo.FileWatcherRecycle/Walterlv.Demo.FileWatcherRecycle/FileSystemWatcherWeakEventRelay.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using Walterlv.WeakEvents;
 
 namespace Walterlv.Demo.FileWatcherRecycle
 {
---
+++ Walterlv.Demo.FileWatcherRecycle/Walterlv.Demo.FileWatcherRecycle/Foo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Walterlv.WeakEvents;
+
+namespace Walterlv.Demo.FileWatcherRecycle
+{
+    public class Foo
+    {
+        private readonly WeakEvent<EventArgs> _bar = new WeakEvent<EventArgs>();
+
+        public event EventHandler Bar
+        {
+            add => _bar.Add(value, value.Invoke);
+            remove => _bar.Remove(value);
+        }
+
+        private void OnBar() => _bar.Invoke(this, EventArgs.Empty);
+    }
+}
