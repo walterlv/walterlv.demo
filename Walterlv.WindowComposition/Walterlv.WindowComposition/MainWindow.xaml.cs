d9d29daa178e1667f3839c9fd9f492fe412c0776
walterlv <xblero@gmail.com>
2019-10-10 12:09:56 +0800

    Add demo window composition.

---
+++ Walterlv.WindowComposition/Walterlv.WindowComposition/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Walterlv.Windows.Effects;
+
+namespace Walterlv.WindowComposition
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var compositor = new WindowAccentCompositor(this);
+            //compositor.Composite(Color.FromArgb(0xa0, 0x18, 0xa0, 0x5e));
+        }
+    }
+}
---
+++ Walterlv.WindowComposition/Walterlv.WindowComposition/WindowAccentCompositor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Walterlv.Windows.Effects
+{
+    /// <summary>
+    /// 为窗口提供模糊特效。
+    /// </summary>
+    public class WindowAccentCompositor
+    {
+        private readonly Window _window;
+
+        /// <summary>
+        /// 创建 <see cref="WindowAccentCompositor"/> 的一个新实例。
+        /// </summary>
+        /// <param name="window">要创建模糊特效的窗口实例。</param>
+        public WindowAccentCompositor(Window window) => _window = window ?? throw new ArgumentNullException(nameof(window));
+
+        public void Composite(Color color)
+        {
+            Window window = _window;
+            var handle = new WindowInteropHelper(window).EnsureHandle();
+
+            var gradientColor =
+                // 组装红色分量。
+                color.R << 0 |
+                // 组装绿色分量。
+                color.G << 8 |
+                // 组装蓝色分量。
+                color.B << 16 |
+                // 组装透明分量。
+                color.A << 24;
+
+            Composite(handle, gradientColor);
+        }
+
+        private void Composite(IntPtr handle, int color)
+        {
+            // 创建 AccentPolicy 对象。
+            var accent = new AccentPolicy
+            {
+                AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND,
+                GradientColor = 0,
+            };
+
+            // 将托管结构转换为非托管对象。
+            var accentPolicySize = Marshal.SizeOf(accent);
+            var accentPtr = Marshal.AllocHGlobal(accentPolicySize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            // 设置窗口组合特性。
+            try
+            {
+                // 设置模糊特效。
+                var data = new WindowCompositionAttributeData
+                {
+                    Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+                    SizeOfData = accentPolicySize,
+                    Data = accentPtr,
+                };
+                SetWindowCompositionAttribute(handle, ref data);
+            }
+            finally
+            {
+                // 释放非托管对象。
+                Marshal.FreeHGlobal(accentPtr);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        private enum AccentState
+        {
+            /// <summary>
+            /// 完全禁用 DWM 的叠加特效。
+            /// </summary>
+            ACCENT_DISABLED = 0,
+
+            /// <summary>
+            /// 
+            /// </summary>
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+            ACCENT_INVALID_STATE = 5,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct AccentPolicy
+        {
+            public AccentState AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct WindowCompositionAttributeData
+        {
+            public WindowCompositionAttribute Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        private enum WindowCompositionAttribute
+        {
+            // 省略其他未使用的字段
+            WCA_ACCENT_POLICY = 19,
+            // 省略其他未使用的字段
+        }
+    }
+}
