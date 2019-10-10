1d7f0f60329ad4bdba1f08fa9d62837234fdfc34
walterlv <xblero@gmail.com>
2019-10-10 12:10:08 +0800

    Add demo window detector.

---
+++ Walterlv.WindowDetector/Walterlv.WindowDetector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Walterlv.WindowDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var windows = WindowEnumerator.FindAll();
+            for (int i = 0; i < windows.Count; i++)
+            {
+                var window = windows[i];
+                Console.WriteLine($@"{i.ToString().PadLeft(3, ' ')}. {window.Title}
+     {window.Bounds.X}, {window.Bounds.Y}, {window.Bounds.Width}, {window.Bounds.Height}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
