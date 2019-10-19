88774f2c3009aa31ce4740d899f6eeb28a926c1c
walterlv <xblero@gmail.com>
2019-10-19 20:43:36 +0800

    Add demo mklink.

---
+++ Walterlv.Demo.MkLink/Walterlv.Demo.MkLink/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Walterlv.Demo.MkLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JunctionPoint.Create("walterlv.demo", @"D:\Developments", true);
+        }
+    }
+}
