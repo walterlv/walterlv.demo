3af317c26f469c2dfe2bae3230d41b53519f750f
walterlv <xblero@gmail.com>
2019-11-12 07:41:52 +0800

    Add hittest demo.

---
+++ Walterlv.Demo.HitTest/Walterlv.Demo.HitTest/DummyPanel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Walterlv.Demo.HitTest
+{
+    public class DummyPanel : UserControl
+    {
+        protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
+        {
+            return null;
+            // return base.HitTestCore(hitTestParameters);
+        }
+    }
+
+    public class DummyChild : UserControl
+    {
+        protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
+        {
+            return null;
+            // return base.HitTestCore(hitTestParameters);
+        }
+    }
+}
