79e976cdcf70e1dafe22bb40857ffea29f9c629a
walterlv <xblero@gmail.com>
2019-12-24 16:46:14 +0800

    Add read write lock

---
+++ Walterlv.Demo.MemoryMappedFile/Walterlv.Demo.MemoryMappedFile/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading.Tasks;
+
+namespace Walterlv.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Task.WaitAll(
+                    Task.Run(Read1Async),
+                    Task.Run(Write1Async)
+                    );
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task Read1Async()
+        {
+            using var token = EnterSafeReadRegion();
+
+            var hasRead = File.ReadAllText("a.md");
+            Console.WriteLine($"读出 {hasRead}");
+        }
+
+        private static async Task Write1Async()
+        {
+            using var token = EnterSafeWriteRegion();
+
+            var toWrite = DateTime.Now.Ticks.ToString();
+            File.WriteAllText("a.md", toWrite);
+
+            Console.WriteLine($"已写 {toWrite}");
+        }
+
+        private static IDisposable EnterSafeReadRegion()
+        {
+            return SafeReadWriteToken.GetForFile("a.md");
+        }
+
+        private static IDisposable EnterSafeWriteRegion()
+        {
+            return SafeReadWriteToken.GetForFile("a.md");
+        }
+    }
+}
