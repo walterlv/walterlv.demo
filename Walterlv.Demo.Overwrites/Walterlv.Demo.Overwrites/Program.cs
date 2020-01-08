8b22ef1cc126cc5b8949869d16becece4c552961
walterlv <xblero@gmail.com>
2020-01-08 11:04:27 +0800

    Add demo.

--- Walterlv.Demo.Overwrites/Walterlv.Demo.Overwrites/Program.cs
+++ Walterlv.Demo.Overwrites/Walterlv.Demo.Overwrites/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Walterlv.Demo.Overwrites
@@ -9,13 +10,14 @@ namespace Walterlv.Demo.Overwrites
     {
         static async Task Main(string[] args)
         {
-            while (true)
+            for (var i = 0; i < int.MaxValue; i++)
             {
+                Console.Write($"[{i.ToString().PadLeft(5, ' ')}]");
                 var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.md");
                 PrepareFile(filePath);
                 await WriteShorterFileAsync(filePath).ConfigureAwait(false);
                 VerifyContents(filePath);
-                Console.ReadLine();
+                //Console.ReadLine();
             }
         }
 
@@ -27,7 +29,10 @@ namespace Walterlv.Demo.Overwrites
         private static async Task WriteShorterFileAsync(string filePath)
         {
             using var fileStream = File.OpenWrite(filePath);
+            //using var fileStream = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite);
             fileStream.SetLength(0);
+            //using var a = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+            //var reader = new StreamReader(a);
             using var stream = new StreamWriter(fileStream, Encoding.UTF8);
             await stream.WriteAsync("oooooooo").ConfigureAwait(false);
         }
@@ -35,6 +40,11 @@ namespace Walterlv.Demo.Overwrites
         private static void VerifyContents(string filePath)
         {
             var text = File.ReadAllText(filePath, Encoding.UTF8);
+            if (text.Length != 8)
+            {
+                Console.WriteLine("长度错误");
+                Console.ReadLine();
+            }
             Console.WriteLine(text);
         }
     }
