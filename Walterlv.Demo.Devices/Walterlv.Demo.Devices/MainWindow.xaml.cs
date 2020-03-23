65cf7c223167c4734b1144709f6febe7b0afd08a
walterlv <xblero@gmail.com>
2020-03-23 15:45:24 +0800

    Add demo device.s

---
+++ Walterlv.Demo.Devices/Walterlv.Demo.Devices/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Walterlv.Demo.Devices
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(5000);
+            GetBiosInformation();
+        }
+
+        private void GetBiosInformation()
+        {
+            string relDt = "";
+            try
+            {
+                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT SerialNumber, SMBIOSBIOSVersion, ReleaseDate FROM Win32_BIOS");
+                ManagementObjectCollection collection = mSearcher.Get();
+                foreach (ManagementObject obj in collection)
+                {
+                    BiosSerialRun.Text = (string)obj["SerialNumber"];
+                    BiosVersionRun.Text = (string)obj["SMBIOSBIOSVersion"];
+                    relDt = (string)obj["ReleaseDate"];
+                    DateTime dt = ManagementDateTimeConverter.ToDateTime(relDt);
+                    BiosDateRun.Text = dt.ToString("dd-MMM-yyyy");//date format
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
